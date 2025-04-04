cc8201ae0be341e732d75a0048474f86633dc11c
apsaavedra <apsaavedramira@gmail.com>
2025-04-04 17:49:20 -0300

    Basic JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+function generateRecommendation(event) {
+  event.preventDefault();
+
+  new Typewriter("#books-recommended", {
+    strings: "Recommendation",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let tropeFormElement = document.querySelector("#trope-form");
+tropeFormElement.addEventListener("submit", generateRecommendation);
 
 let prompt = "";
 let context = "You are IA specialized in romance books.";
 let apiKey = "39d5a6021fofb45e9e8c92202c37tc3f";
-let apiUrl =
-  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(showRecommendation)
+axios.get(apiUrl).then(showRecommendation);
